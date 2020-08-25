915cb284f56236e7574057b08a11e13281ff0f59
AltTether <245134e@gs.kochi-tech.ac.jp>
2020-08-26 03:46:06 +0900

    serverのテストを作成

---
+++ server/server_test.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"bytes"
+	"testing"
+	"net/http"
+	"encoding/json"
+	"net/http/httptest"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	testMessage = "test"
+	testRequestBody = &RequestBody{
+		Message: &testMessage,
+	}
+)
+
+func TestGetBottleRouteEmpty(t *testing.T) {
+	r := New()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/bottle", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, "", w.Body.String())
+}
+
+func TestPostBottleRoute(t *testing.T) {
+	r := New()
+
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(testRequestBody)
+	req, _ := http.NewRequest("POST",
+		"/api/v1/bottle",
+		bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "", w.Body.String())
+}
+
+func TestGetBottleRoute(t *testing.T) {
+	r := New()
+
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(testRequestBody)
+	req, _ := http.NewRequest("POST",
+		"/api/v1/bottle",
+		bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+
+	req, _ = http.NewRequest("GET", "/api/v1/bottle", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"message\":{\"text\":\"test\"}}", w.Body.String())
+}
