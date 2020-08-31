a56e12b3cc79b034d4899bc5cfdcf74225b89002
AltTether <245134e@gs.kochi-tech.ac.jp>
2020-09-01 06:04:17 +0900

    engineのRunを修正可能なように書き換える

--- bottles/server.go
+++ bottles/server.go
@@ -1,7 +1,11 @@
 package bottles
 
 import(
+	"os"
+	"fmt"
 	"time"
+	"strings"
+	"net/http"
 	
 	"github.com/gin-gonic/gin"
 )
@@ -72,5 +76,47 @@ func registerRoute(r *Engine, getPipeline, postPipeline *Pipeline) {
 		v1.GET("/bottle", GetBottleHandlerFunc(getPipeline))
 		v1.POST("/bottle", PostBottleHandlerFunc(postPipeline))
 		v1.GET("/bottle/stream", GetBottleStreamHandlerFunc(getPipeline))
+	}
+}
+
+func (engine *Engine) Run(addr ...string) (err error) {
+	defer func() { debugPrintError(err) }()
+
+	address := resolveAddress(addr)
+	debugPrint("Listening and serving HTTP on %s\n", address)
+	err = http.ListenAndServe(address, engine)
+	return
+}
+
+func debugPrintError(err error) {
+	if err != nil {
+		if gin.IsDebugging() {
+			fmt.Fprintf(gin.DefaultErrorWriter, "[GIN-debug] [ERROR] %v\n", err)
+		}
+	}
+}
+
+func debugPrint(format string, values ...interface{}) {
+	if gin.IsDebugging() {
+		if !strings.HasSuffix(format, "\n") {
+			format += "\n"
+		}
+		fmt.Fprintf(gin.DefaultWriter, "[GIN-debug] "+format, values...)
+	}
+}
+
+func resolveAddress(addr []string) string {
+	switch len(addr) {
+	case 0:
+		if port := os.Getenv("PORT"); port != "" {
+			debugPrint("Environment variable PORT=\"%s\"", port)
+			return ":" + port
+		}
+		debugPrint("Environment variable PORT is undefined. Using port :8080 by default")
+		return ":8080"
+	case 1:
+		return addr[0]
+	default:
+		panic("too many parameters")
 	}
 }
